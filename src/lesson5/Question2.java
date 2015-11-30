a24317c7f6cfbd758c925d4817e5dae6e8aabb79
Prabhat Poudel <prabhat.poudl@gmail.com>
2015-11-30 16:25:02 -0600

    Signed-off-by: Prabhat Poudel <prabhat.poudl@gmail.com>

---
+++ src/lesson5/Question2.java
@@ -0,0 +1,35 @@
+package lesson5;
+
+class Computer
+{
+	String manufacturer;
+	String processor;
+	int	ramSize;
+	int diskSize;
+	double processorSpeed;
+	
+	double getRamSize()
+	{
+		
+	}
+	double getDiskSize()
+	{
+	
+	}
+	double getProcessorSpeed()
+	{
+		
+	}
+	double computePower() 
+	{
+		// return ramSize multiplied by processorSpeed
+	}
+}
+
+class NoteBook extends Computer
+{
+	
+}
+public class Question2 {
+
+}
