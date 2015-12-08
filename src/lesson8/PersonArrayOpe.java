82d3d64bd8e59595ece31a30863c1a802dd1b194
Prabhat Poudel <prabhat.poudl@gmail.com>
2015-12-08 14:15:23 -0600

    Signed-off-by: Prabhat Poudel <prabhat.poudl@gmail.com>

--- src/lesson8/PersonArrayOpe.java
+++ src/lesson8/PersonArrayOpe.java
@@ -14,32 +14,32 @@ class Person {
 
 	public void displayPerson() // Display fisrtName, lastName and age
 	{
-		System.out.println("Age of the Person is: "+age);
+		System.out.println("LastName is : "+lastName+ "\t FirstName is :" +firstName+ " \t Age of the Person is: "+age);
 	}
 
 	public String getLastName() // get only the last name
 	{
-		return lastName;
+		return this.lastName;
 	}
 } // end class Person
 
 public class PersonArrayOpe {
 	private Person[] a; // reference to array
-	private int nElems; // number of data items
+//	private int nElems; // number of data items
 	private int size;
 
 	public PersonArrayOpe(int max) // constructor to initialize person array  with size max and nElems;
 	{
 		a = new Person[max];
 		size= 0;
-		nElems ++;
+//		nElems ++;
 	}
 
 	public Person find(String searchName) // Pass Lastname as an argument
 	{
 		for(Person p : a)
 		{
-			if(p.equals(searchName))
+			if(p.getLastName().equals(searchName))
 				return p;
 		}
 		return null;
@@ -60,12 +60,13 @@ public class PersonArrayOpe {
 		if(size == 0) return false;
 		int index = -1;
 		for(int i = 0; i < size; ++i ){
-			System.out.println(a[i].getLastName());
-			if(a[i].equals(searchName)){
+//			System.out.println(a[i].getLastName());
+			if(a[i].getLastName().equals(searchName)){
 				index = i;
 				break;
 			}
 		}
+
 		if(index==-1) return false;
 		Person[] temp = new Person[a.length];
 		System.arraycopy(a,0,temp,0,index);
@@ -76,11 +77,16 @@ public class PersonArrayOpe {
 	}
 
 	public void displayAll() // displays array contents
-	{
-		for(Person p: a)
-		{
-			System.out.println(p);
-		}
+	{ 
+       for(int i =0; i<size; i++)
+       {
+    	   a[i].displayPerson();
+       }
+//		for(Person p: a)
+//		{
+//			p.displayPerson();
+//			System.out.println(p.toString());
+//		}
 	}
 
 	public int size() // Return the number of persons stored in the array
@@ -105,23 +111,27 @@ public class PersonArrayOpe {
 		
 		return sb.toString();
 	}
-	
-
 
 public static void main(String[] args) {
-	PersonArrayOpe p = new PersonArrayOpe(1);
+	PersonArrayOpe p = new PersonArrayOpe(2);
 	p.insert("Poudel", "Prabhat", 28);
 	p.insert("Shakya", "Gunjan", 29);
 	
+	
+	
 //System.out.println("The list of size "+p.size());
     p.displayAll();
+    System.out.println("************");
 	p.delete("Shakya");
     p.displayAll();
+    System.out.println("************");
 	//System.out.println("The list of size1  "+p.size()+" is "+p);
 	p.insert("Smith", "Joe", 30);
-//    p.displayAll();
+    p.displayAll();
 	//System.out.println("The list of size2  "+p.size()+" after "+p);
-	
+    System.out.println("************");
+	Person p1 = p.find("Poudel");
+	p1.displayPerson();
 	
 	
 }
